9c30bc5fe5642eb188b9940f7b7d07fa8bc374d7
MathewReiss <MathewReiss@users.noreply.github.com>
2019-08-15 13:08:32 -0400

    form

--- index.php
+++ index.php
@@ -91,29 +91,173 @@
         <div class="row">
           <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
             <h2 class="mb-5">Step 1. Fill out the form</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nam obcaecati fuga itaque deserunt officia, error reiciendis ab quod?</p>
+            <p>Select Language: 
+            	<select class="form-control border-secondary text-white bg-transparent" name="lang" id="lang">
+            		<option value="en" default>English</option>
+            		<option value="kr">Korean</option>
+            	</select>
+            </p>
+            <p>What team do you currently play for? 
+            	<select class="form-control border-secondary text-white bg-transparent" name="team" id="team">
+            		<option value="atl">Atlanta Reign</option>
+            		<option value="bos">Boston Uprising</option>
+            		<option value="cdh">Chengdu Hunters</option>
+            		<option value="dal">Dallas Fuel</option>
+            		<option value="fla">Florida Mayhem</option>
+            		<option value="gzc">Guangzhou Charge</option>
+            		<option value="hzs">Hangzhou Spark</option>
+            		<option value="hou">Houston Outlaws</option>
+            		<option value="lon">London Spitfire</option>
+            		<option value="lag">Los Angeles Gladiators</option>
+            		<option value="lav">Los Angeles Valiant</option>
+            		<option value="nyxl">New York Excelsior</option>
+            		<option value="par">Paris Eternal</option>
+            		<option value="phi">Philadelphia Fusion</option>
+            		<option value="sfs">San Francisco Shock</option>
+            		<option value="seo">Seoul Dynasty</option>
+            		<option value="shd">Shanghai Dragons</option>
+            		<option value="tor">Toronot Defiant</option>
+            		<option value="van">Vancouver Titans</option>
+            		<option value="was">Washington Justice</option>
+            	</select>
+            </p>
+            <p>Why are you retiring?<br />
+            	<select class="form-control border-secondary text-white bg-transparent" name="why" id="why">
+            		<option value="stream">Made more money streaming $$$</option>
+            		<option value="cut">Probably going to be cut anyways</option>
+            		<option value="other">Going Pro in Fortnite / APEX / Minecraft</option>
+            		<option value="la">I hate living in Los Angeles</option>
+            		<option value="peak">I've peaked</option>
+            		<option value="boston">I'm never going to be as cool as the Boston Uprising</option>
+            		<option value="local">I can't afford to live in my new city now that home games are a thing</option>
+            		<option value="scandal">There's a scandal about me just waiting to get leaked so I'm getting ahead of it by retiring quietly</option>
+            		<option value="age">Aged out. I'm too old for this s***.</option>
+            	</select>
+            </p>
+            <p>How did you get your start in OWL?<br />
+            	<select class="form-control border-secondary text-white bg-transparent" name="start" id="start">
+            		<option value="legion">Gladiators Legion</option>
+            		<option value="contenders">Some other Contenders team</option>
+            		<option value="ladder">Ladder (git gud, scrub)</option>
+            		<option value="uncle">My uncle works at Blizzard</option>
+            		<option value="world">World Cup</option>
+            		<option value="lost">I got lost wandering around Blizzard Area and accidentally ended up onstage</option>
+            		<option value="two">Two-way contract</option>
+            	</select>
+            </p>
+            <p>Highlight of your OWL career?<br />
+            	<select class="form-control border-secondary text-white bg-transparent" name="highlight" id="highlight">
+            		<option value="brig">Playing my favorite hero - Brig</option>
+            		<option value="backstage">Getting to sit backstage during that epic reverse sweep</option>
+            		<option value="allstar">The All Star Weekend that nobody watched</option>
+            		<option value="potg">That time I got POTG and you could *almost* see it on stream</option>
+            		<option value="danny">That time I touched Danny's mic</option>
+            		<option value="mvp">That time I got League MVP even though Twilight definitely deserved it more</option>
+            	</select>
+            </p>
+            <p>What's something you'll miss?<br />
+            	<select class="form-control border-secondary text-white bg-transparent" name="miss" id="miss">
+            		<option value="awkward">That awkward moment deciding whether to bow, shake hands, or hug after a match</option>
+            		<option value="casters">The casters mispronouncing my gamertag</option>
+            		<option value="hands">Those hand-warmy packets</option>
+            		<option value="swag">The free swag we get from the team sponsor</option>
+            		<option value="soe">Soe</option>
+            		<option value="meta">The meta changing drastically mid-season. Yeah, that was fun.</option>
+            	</select>
+            </p><br />
+            <button class="btn btn-secondary rounded-top-right-0" type="button" id="button-generate" onclick="generateText()">Generate Message</button>            
           </div>
         </div>
       </div>
     </div>
+
+    <script>
+    	function generateText(){
+    		var lang = document.getElementById("lang").value;
+    		var team = document.getElementById("team").value;
+    		var why = document.getElementById("why").value;
+    		var start = document.getElementById("start").value;
+    		var highlight = document.getElementById("highlight").value;
+    		var miss = document.getElementById("miss").value;
+
+    		var generated = document.getElementById("generated");
+    		generated.innerHTML = "";
+
+    		// Greeting
+    		
+    		generated.innerHTML += (lang == "en" ? "hi everyone" : "여러분 안녕");
+
+    		generated.innerHTML += "<br /><br />";
+
+    		// Introduction
+
+    		generated.innerHTML += "";
+
+    		generated.innerHTML += "<br /><br />";
+
+    		// Reason for retiring
+
+    		generated.innerHTML += "";
+
+    		generated.innerHTML += "<br /><br />";
+
+    		// Nostalgia
+
+			generated.innerHTML += "";
+
+    		generated.innerHTML += "<br /><br />";
+
+    		// Highlight
+
+    		generated.innerHTML += "";
+
+    		generated.innerHTML += "<br /><br />";
+
+    		// Something worth missing
+
+    		generated.innerHTML += "";
+
+    		var generateButton = document.getElementById("button-generate");
+    		generateButton.innerHTML = "Generated!";
+    	}
+    </script>
 
     <div class="site-section">
       <div class="container" data-aos="fade-up">
         <div class="row">
           <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
             <h2 class="mb-5">Step 2. Copy to Clipboard</h2>
-            <p>You've worked hard this past season. Your team did well. Or it didn't. But you realize you've had enough. It's been a good run, and it's important to know when to call it quits. That's right, you're going to retire from the Overwatch League.</p>
+            <p id="generated">
+            <em>Generated text will go here...</em>
+            </p><br />
+            <button class="btn btn-secondary rounded-top-right-0" type="button" id="button-copy" onclick="copyToClipboard()">Copy to Clipboard</button>
           </div>
         </div>
 	  </div>
 	</div>
+
+	<script>
+		function copyToClipboard(){
+			var generated = document.getElementById("generated");		
+
+			var inp = document.createElement('input');
+			document.body.appendChild(inp);
+			inp.value = generated.textContent;
+			inp.select();
+			document.execCommand('copy', false);
+			inp.remove();
+
+			var copyButton = document.getElementById("button-copy");
+			copyButton.innerHTML = "Copied!";			
+		}
+	</script>
 
     <div class="site-section bg-dark">
       <div class="container" data-aos="fade-up">
         <div class="row">
           <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
             <h2 class="mb-5">Step 3. Create Twitlonger</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nam obcaecati fuga itaque deserunt officia, error reiciendis ab quod?</p>
+            <a href="https://www.twitlonger.com/post" target="_blank"><button class="btn btn-secondary rounded-top-right-0" type="button" id="button-addon2">Write a Post</button></a>
           </div>
         </div>
       </div>
@@ -124,7 +268,7 @@
         <div class="row">
           <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
             <h2 class="mb-5">Step 4. Post to Twitter</h2>
-            <p>You've worked hard this past season. Your team did well. Or it didn't. But you realize you've had enough. It's been a good run, and it's important to know when to call it quits. That's right, you're going to retire from the Overwatch League.</p>
+            <a href="https://twitter.com/compose/tweet?text=retiring+from+OW%0a%0aread+more%3A+%3C%3CTWITLONGER+LINK+GOES+HERE%3E%3E" target="_blank"><button class="btn btn-secondary rounded-top-right-0" type="button" id="button-addon2">Compose a Tweet</button></a>
           </div>
         </div>
 	  </div>
@@ -134,16 +278,16 @@
       <div class="container" data-aos="fade-up">
         <div class="row">
           <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
-            <h2 class="mb-5">Step 5. Let Your New Life Begin</h2>
-            <p>That's it! Now you're free. Maybe take the time to play a game <em>other</em> than Overwatch, or catch up on your <a href="https://www.soundcloud.com/weeklyuprising" target="_blank">favorite podcast</a>.
-
+            <h2 class="mb-5">Step 5. Live Your New Life</h2>
+            <p>That's it! You're free!! Maybe take the time to play a game <em>other</em> than Overwatch, or catch up on your <a href="https://www.soundcloud.com/weeklyuprising" target="_blank">favorite podcast</a>.
+            	<br /><br />
             Either way, congratulations! You've just retired from the Overwatch League.
 
         	Where you were paid.
 
     		To play video games.</p><br />
-    		<a href="https://help.twitter.com/en/using-twitter/delete-tweets" target="_blank"><button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2">Oh God, What Have I Done?!?</button></a><br /><br />
-    		<a href="https://www.aarp.org/retirement/planning-for-retirement/info-2019/do-and-dont.html" target="_blank"><button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2">Now I Can Rest</button></a>
+    		<a href="https://help.twitter.com/en/using-twitter/delete-tweets" target="_blank"><button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2"><strong>Oh God, What Have I Done?!?</strong></button></a><br /><br />
+    		<a href="https://www.aarp.org/retirement/planning-for-retirement/info-2019/do-and-dont.html" target="_blank"><button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2"><strong>Now I Can Rest</strong></button></a>
           </div>
         </div>
       </div>
@@ -155,7 +299,7 @@
           <div class="col-lg-4">
             <div class="mb-5">
               <h3 class="footer-heading mb-4">About OW Retirement Machine</h3>
-              <p>Because the end of the season is drawing near, and you never know who will or won't survive the "Hukening" (at least on Boston), I created this helpful tool for players who needed a little help expressing themselves. /s<br /><br />OW Retirement Machine was started as a joke after one of the hosts of the <a href="https://www.twitter.com/weeklyuprising" target="_blank">Weekly Uprising Podcast</a> commented that players use the word "retire" somewhat casually in esports compared to traditional sports.  I hope it gave you a laugh, and if you like this kind of thing, check out the <a href="https://www.soundcloud.com/weeklyuprising" target="_blank">podcast</a>!<br />-Snowy</p>
+              <p>Because the end of the season is drawing near, and you never know who will or won't survive the "Hukening" (at least on Boston), I created this helpful tool for players who needed a little help expressing themselves. /s<br /><br />OW Retirement Machine was started as a joke after one of the hosts of the <a href="https://www.twitter.com/weeklyuprising" target="_blank">Weekly Uprising Podcast</a> commented that players use the word "retire" somewhat casually in esports compared to traditional sports.  I hope it gave you a laugh, and if you like this kind of thing, check out the <a href="https://www.soundcloud.com/weeklyuprising" target="_blank">podcast</a>!<br /><span style="text-align: right;">-<em>Snowy</em></span><br /><br />Oh! And the name is obviously a reference to <a href="http://owplayoffmachine.com/" target="_blank">OW Playoff Machine</a>, a great website that lets you know just how early your team has been eliminated from playoff contention :)</p>
             </div>
             <div class="mb-5">
               <h3 class="footer-heading mb-4">Subscribe</h3>
